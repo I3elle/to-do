ebabe704be60115dfc34dc3f8eecac86b4605d36
andrea.bellei <andrea.bellei@fermi.mo.it>
2024-07-30 16:46:24 +0200

    authentication routing base model

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,10 +3,28 @@ import { createBrowserRouter } from 'react-router-dom'
 import { lazy } from 'react'
 
 const Initial = lazy(()=>import('./components/pages/initial/initial'))
+const isAuthenticated = false
 
-export const router = createBrowserRouter([
+const not_authenticated= [
     {
         path:"/",
         element: <Initial />
+    },
+    {
+        path:"sing-in/"
+        
+    },
+    {
+        path:"register/"
     }
-])
+
+]
+
+const authenticated = [
+
+]
+
+export const router = createBrowserRouter(isAuthenticated ? authenticated : not_authenticated)
+
+
+
