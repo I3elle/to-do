e9b4c73205d488c2daba651cd8c3341303a7799d
andrea.bellei <andrea.bellei@fermi.mo.it>
2024-07-30 16:33:23 +0200

    initial page background component

---
+++ frontend/src/components/comps/initial-bg.jsx
@@ -0,0 +1,10 @@
+
+export default function Background(){
+    return(
+        <div className="background">
+            <div className="circle"></div>
+            <div className="diagonal-lines"></div>
+        </div>
+    )
+}
+        
