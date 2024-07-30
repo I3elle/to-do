649826302fc67f51e79252fe60182129dfb5d63b
andrea.bellei <andrea.bellei@fermi.mo.it>
2024-07-30 16:31:02 +0200

    installet new packages and included some style files (including tailwind)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+          "general-bg":"#f6f6f6",
+          "details":"#930000",
+          "text-detail":"#979797"
+      }
+    },
+  },
+  plugins: [],
+}
+
