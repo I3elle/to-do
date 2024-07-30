7cf2aa948a3360719b1ddfc5cfbe82b17974e0ca
andrea.bellei <andrea.bellei@fermi.mo.it>
2024-07-30 16:34:39 +0200

    addded random phrases for showing them to the user

---
+++ frontend/src/components/pages/initial/phrases.js
@@ -0,0 +1,22 @@
+export const phrases = [
+    {
+        "phrase":"Any fun missions on your to-do list? 😊",
+        "colored-word": "fun",
+        "color": "#486000"
+    },
+    {
+        "phrase": "What exciting quests are you embarking on today?🏊‍♀️",
+        "colored-word": "quests",
+        "color": "#FBF198"
+    },
+    {
+        "phrase": "What's on your plate for today's menu of activities?🧗‍♂️‍",
+        "colored-word": "activities",
+        "color": "#49CC93"
+    },
+    {
+        "phrase": "What adventures await you today?🗡️‍",
+        "colored-word": "adventures",
+        "color": "#7E84FC"
+    }
+]
